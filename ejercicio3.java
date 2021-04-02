2473d394ef9382e24fab398e1130f5f222447fc4
AzureAD\NataliaCarolinaBueno <ncb1985@gmail.com>
2021-04-02 10:57:25 -0300

    Se sube ejercicio 1 del practico 3

---
+++ ejercicio3.java
@@ -0,0 +1,12 @@
+package Practico6;
+
+public class ejercicio3 {
+    public static void main (String args[]){
+        int x= 4;
+        System.out.println("El doble de "+ x+" es "+ elDobleDeX(x));
+    }
+
+    public static int elDobleDeX(int x){
+        return (x*2);
+    }
+}
